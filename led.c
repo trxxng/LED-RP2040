4e40f0b2c1a0d0d24a8b7036fbb2e60c9f7b4cfc
trxxng <truong.nv226464@sis.hust.edu.vn>
2025-07-25 17:30:17 +0700

    folder

--- led.c
+++ led.c
@@ -1,11 +1,11 @@
 #include "pico/stdlib.h"
 int main() {
-    gpio_init(25); // Initialize GPIO 25
-    gpio_set_dir(25, GPIO_OUT); // Set GPIO 25 as output
+    gpio_init(25); // Khởi tạo GPIO25
+    gpio_set_dir(25, GPIO_OUT); // Set GPIO25 output
     while(1){
-        gpio_put(25, 1); // Set GPIO 25 high
+        gpio_put(25, 1); // Set GPIO25 high
         sleep_ms(1000);
-        gpio_put(25, 0); // Set GPIO 25 low
+        gpio_put(25, 0); // Set GPIO25 low
         sleep_ms(1000);
     }
 }
